06701e28f1ce7213f93505c9c0ef52125211ee88
Daniel Deiana <danieldeiana84@gmail.com>
2018-07-14 12:41:28 +0100

    boilerplate2

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
